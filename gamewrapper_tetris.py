87bdd5b05b3d541091a2ccf3a1b04dec0104025a
11ray <raruidol@dsic.upv.es>
2020-05-03 20:01:37 +0200

    PyTorch friendship cancelled, Keras is the new best friend

--- gamewrapper_tetris.py
+++ gamewrapper_tetris.py
@@ -1,84 +1,18 @@
 import numpy as np
 import random
-import math
-import os
-import matplotlib.pyplot as plt
-from collections import namedtuple
-import torch as nn
-import torch
-import torch.nn as nn
-import torch.optim as opt
-import torch.nn.functional as F
-import torchvision.transforms as T
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.optimizers import Adam
+import gym
 from pyboy import PyBoy, WindowEvent
-from itertools import count
+from collections import deque
+
 
 filename = 'ROMs/Tetris_World.gb'
-
 quiet = True
 pyboy = PyBoy(filename, window_type="headless" if quiet else "SDL2", window_scale=3, debug=not quiet, game_wrapper=True)
 pyboy.set_emulation_speed(0)
 tetris = pyboy.game_wrapper()
-tetris.start_game()
-last_fitness = 0
-print(tetris)
-
-BATCH_SIZE = 128
-GAMMA = 0.999
-EPS_START = 0.9
-EPS_END = 0.05
-EPS_DECAY = 200
-TARGET_UPDATE = 10
-
-
-class ReplayMemory(object):
-
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.memory = []
-        self.position = 0
-
-    def push(self, *args):
-        """Saves a transition."""
-        if len(self.memory) < self.capacity:
-            self.memory.append(None)
-        self.memory[self.position] = Transition(*args)
-        self.position = (self.position + 1) % self.capacity
-
-    def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
-
-    def __len__(self):
-        return len(self.memory)
-
-
-class DQN(nn.Module):
-
-    def __init__(self, h, w, outputs):
-        super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=5, stride=2)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-        self.bn3 = nn.BatchNorm2d(32)
-
-        # Number of Linear input connections depends on output of conv2d layers
-        # and therefore the input image size, so compute it.
-        def conv2d_size_out(size, kernel_size = 5, stride = 2):
-            return (size - (kernel_size - 1) - 1) // stride  + 1
-        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
-        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
-        linear_input_size = convw * convh * 32
-        self.head = nn.Linear(linear_input_size, outputs)
-
-    # Called with either one element to determine next action, or a batch
-    # during optimization. Returns tensor([[left0exp,right0exp]...]).
-    def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        return self.head(x.view(x.size(0), -1))
 
 
 def press_right():
@@ -91,130 +25,127 @@ def press_left():
     pyboy.send_input(WindowEvent.RELEASE_ARROW_LEFT)
 
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 ACTIONS = [press_right(), press_left()]
 
 
-init_screen = torch.from_numpy(np.asarray(tetris.game_area(), dtype='float32')).to(device)
-screen_height, screen_width = init_screen.shape
-
-n_actions = len(ACTIONS)
-
-policy_net = DQN(screen_height, screen_width, n_actions).to(device)
-target_net = DQN(screen_height, screen_width, n_actions).to(device)
-target_net.load_state_dict(policy_net.state_dict())
-target_net.eval()
-
-optimizer = opt.RMSprop(policy_net.parameters())
-memory = ReplayMemory(10000)
+def get_state(state):
+    st = np.asarray(state, dtype='float32')
+    return st.reshape(1,180)
 
 
-steps_done = 0
-episode_durations = []
+class DQN:
+    def __init__(self, env):
+        self.env = env
+        self.memory = deque(maxlen=2000)
 
-def select_action(state):
-    global steps_done
-    sample = random.random()
-    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
-        math.exp(-1. * steps_done / EPS_DECAY)
-    steps_done += 1
-    if sample > eps_threshold:
-        with torch.no_grad():
-            # t.max(1) will return largest column value of each row.
-            # second column on max result is index of where max element was
-            # found, so we pick action with the larger expected reward.
-            return policy_net(state).max(1)[1].view(1, 1)
-    else:
-        return torch.tensor([[random.randrange(n_actions)]], device=device, dtype=torch.long)
+        self.gamma = 0.85
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.005
+        self.tau = .125
+
+        self.model = self.create_model()
+        self.target_model = self.create_model()
+
+    def create_model(self):
+        model = Sequential()
+        state_shape = self.env.game_area().shape
+        model.add(Dense(24, input_shape=(180, ), activation="relu"))
+        model.add(Dense(48, activation="relu"))
+        model.add(Dense(24, activation="relu"))
+        model.add(Dense(len(ACTIONS)))
+        model.summary()
+        model.compile(loss="mean_squared_error",
+                      optimizer=Adam(lr=self.learning_rate))
+
+        return model
+
+    def act(self, state):
+        self.epsilon *= self.epsilon_decay
+        self.epsilon = max(self.epsilon_min, self.epsilon)
+        if np.random.random() < self.epsilon:
+            return ACTIONS[0]
+        pred = self.model.predict(state)
+        return np.argmax(pred[0])
+
+    def remember(self, state, action, reward, new_state, done):
+        self.memory.append([state, action, reward, new_state, done])
+
+    def replay(self):
+        batch_size = 32
+        if len(self.memory) < batch_size:
+            return
+
+        samples = random.sample(self.memory, batch_size)
+        for sample in samples:
+            state, action, reward, new_state, done = sample
+            target = self.target_model.predict(state)
+            if done:
+                target[0][action] = reward
+            else:
+                Q_future = max(self.target_model.predict(new_state)[0])
+                target[0][action] = reward + Q_future * self.gamma
+            self.model.fit(state, target, epochs=1, verbose=0)
+
+    def target_train(self):
+        weights = self.model.get_weights()
+        target_weights = self.target_model.get_weights()
+        for i in range(len(target_weights)):
+            target_weights[i] = weights[i] * self.tau + target_weights[i] * (1 - self.tau)
+        self.target_model.set_weights(target_weights)
+
+    def save_model(self, fn):
+        self.model.save(fn)
 
 
-def optimize_model():
-    if len(memory) < BATCH_SIZE:
-        return
-    transitions = memory.sample(BATCH_SIZE)
-    # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
-    # detailed explanation). This converts batch-array of Transitions
-    # to Transition of batch-arrays.
-    batch = Transition(*zip(*transitions))
+def main():
 
-    # Compute a mask of non-final states and concatenate the batch elements
-    # (a final state would've been the one after which simulation ended)
-    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-                                          batch.next_state)), device=device, dtype=torch.bool)
-    non_final_next_states = torch.cat([s for s in batch.next_state
-                                                if s is not None])
-    state_batch = torch.cat(batch.state)
-    action_batch = torch.cat(batch.action)
-    reward_batch = torch.cat(batch.reward)
+    tetris.start_game()
 
-    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
-    # columns of actions taken. These are the actions which would've been taken
-    # for each batch state according to policy_net
-    state_action_values = policy_net(state_batch).gather(1, action_batch)
+    gamma = 0.9
+    epsilon = .95
 
-    # Compute V(s_{t+1}) for all next states.
-    # Expected values of actions for non_final_next_states are computed based
-    # on the "older" target_net; selecting their best reward with max(1)[0].
-    # This is merged based on the mask, such that we'll have either the expected
-    # state value or 0 in case the state was final.
-    next_state_values = torch.zeros(BATCH_SIZE, device=device)
-    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
-    # Compute the expected Q values
-    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+    trials = 1000
+    trial_len = 500
 
-    # Compute Huber loss
-    loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+    # updateTargetNetwork = 1000
+    dqn_agent = DQN(env=tetris)
+    steps = []
+    for trial in range(trials):
+        cur_state = get_state(tetris.game_area())
 
-    # Optimize the model
-    optimizer.zero_grad()
-    loss.backward()
-    for param in policy_net.parameters():
-        param.grad.data.clamp_(-1, 1)
-    optimizer.step()
+        #print(cur_state)
+        for step in range(trial_len):
+            action = dqn_agent.act(cur_state)
 
+            # En cada paso recoger el nuevo estado, la reward y la comprovación de si está done la partida
+            pyboy.tick()
+            new_state = get_state(tetris.game_area())
+            reward = tetris.score
+            done = pyboy.tick()
+            #new_state, reward, done, _ = env.step(action)
+            #print(new_state, reward, done)
 
-num_episodes = 50
-for i_episode in range(num_episodes):
-    # Initialize the environment and state
-    tetris.reset_game()
-    last_screen = torch.from_numpy(np.asarray(tetris.game_area(), dtype='float32')).to(device)
-    current_screen = torch.from_numpy(np.asarray(tetris.game_area(), dtype='float32')).to(device)
-    state = current_screen - last_screen
-    done = not pyboy.tick()
-    for t in count():
-        # Select and perform an action
-        action = select_action(state)
-        ACTIONS[action]
-        reward = tetris.score
-        reward = torch.tensor([reward], device=device)
+            # reward = reward if not done else -20
+            #new_state = new_state.reshape(1, 2)
+            dqn_agent.remember(cur_state, action, reward, new_state, done)
 
-        # Observe new state
-        last_screen = current_screen
-        current_screen = torch.from_numpy(np.asarray(tetris.game_area(), dtype='float32')).to(device)
-        if not done:
-            next_state = current_screen - last_screen
+            dqn_agent.replay()  # internally iterates default (prediction) model
+            dqn_agent.target_train()  # iterates target model
+
+            cur_state = new_state
+            if done:
+                break
+        if step >= 199:
+            print("Failed to complete in trial {}".format(trial))
+            if step % 10 == 0:
+                dqn_agent.save_model("trial-{}.model".format(trial))
         else:
-            next_state = None
-
-        # Store the transition in memory
-        memory.push(state, action, next_state, reward)
-
-        # Move to the next state
-        state = next_state
-
-        # Perform one step of the optimization (on the target network)
-        optimize_model()
-        if done:
-            episode_durations.append(t + 1)
-            #plot_durations()
+            print("Completed in {} trials".format(trial))
+            dqn_agent.save_model("success.model")
             break
-    # Update the target network, copying all weights and biases in DQN
-    if i_episode % TARGET_UPDATE == 0:
-        target_net.load_state_dict(policy_net.state_dict())
-
-print('Complete')
-tetris.reset_game()
-pyboy.stop()
 
 
+if __name__ == "__main__":
+    main()
